ee8ad4e36ccc4c23f724f4858065902ba269fefc
Sam Dickinson <sam.dickinson@shinesolutions.com.au>
2025-04-22 13:05:13 +1000

    Initial

---
+++ index.test.ts
@@ -0,0 +1,95 @@
+import { describe, expect, it, vi } from 'vitest';
+import { startLog } from '.';
+import pino, { Level } from 'pino';
+describe('Logger Tests', () => {
+  it('should output when out of scope', () => {
+    // ARRANGE
+    const testTime = new Date();
+    vi.setSystemTime(testTime);
+    let pinoLogger: pino.Logger;
+    {
+      using log = startLog('TestEvent');
+      pinoLogger = log.logger;
+      pinoLogger.info = vi.fn();
+      log.log({ test: 'test' });
+      // ACT
+    }
+    // ASSERT
+    expect(pinoLogger.info).toHaveBeenCalledExactlyOnceWith({
+      startTime: testTime,
+      endTime: testTime,
+      totalTime: 0,
+      event: 'TestEvent',
+      test: 'test',
+    });
+  });
+  it('should be able to turn existing data to an array', () => {
+    // ARRANGE
+    const testTime = new Date();
+    vi.setSystemTime(testTime);
+    let pinoLogger: pino.Logger;
+    {
+      using log = startLog('TestEvent');
+      pinoLogger = log.logger;
+      pinoLogger.info = vi.fn();
+      log.log({ test: 'test' });
+      log.log({ test: 'test2' });
+      log.log({ test: 'test3' });
+      // ACT
+    }
+    // ASSERT
+    expect(pinoLogger.info).toHaveBeenCalledExactlyOnceWith({
+      startTime: testTime,
+      endTime: testTime,
+      totalTime: 0,
+      event: 'TestEvent',
+      test: ['test', 'test2', 'test3'],
+    });
+  });
+  const levels: Level[] = ['trace', 'debug', 'info', 'warn', 'error', 'fatal'];
+  it.each(levels)('should be able to test at level %s', (level) => {
+    // ARRANGE
+    const testTime = new Date();
+    vi.setSystemTime(testTime);
+    let pinoLogger: pino.Logger;
+    {
+      using log = startLog('TestEvent');
+      pinoLogger = log.logger;
+      pinoLogger[level] = vi.fn();
+      log[level]({ test: 'test' });
+      // ACT
+    }
+    // ASSERT
+    expect(pinoLogger[level]).toHaveBeenCalledExactlyOnceWith({
+      startTime: testTime,
+      endTime: testTime,
+      totalTime: 0,
+      event: 'TestEvent',
+      test: 'test',
+    });
+  });
+  it.each(levels)(
+    'should be able to test at level with default level %s',
+    (level) => {
+      // ARRANGE
+      const testTime = new Date();
+      vi.setSystemTime(testTime);
+      let pinoLogger: pino.Logger;
+      {
+        using log = startLog('TestEvent', level);
+        pinoLogger = log.logger;
+        pinoLogger[level] = vi.fn();
+        log.log({ test: 'test' });
+        // ACT
+      }
+      // ASSERT
+      expect(pinoLogger[level]).toHaveBeenCalledExactlyOnceWith({
+        startTime: testTime,
+        endTime: testTime,
+        totalTime: 0,
+        event: 'TestEvent',
+        test: 'test',
+      });
+    },
+  );
+});
---
+++ index.ts
@@ -0,0 +1,115 @@
+import pino from 'pino';
+
+let globalLogger: pino.Logger | undefined;
+export const startLog = (
+  event: string,
+  defaultLogLevel: pino.Level = 'info',
+) => {
+  if (!globalLogger) {
+    globalLogger = pino();
+  }
+  return new Logger(event, defaultLogLevel);
+};
+
+class Logger implements Disposable {
+  logger: pino.Logger;
+  #currentLogLevel: pino.Level;
+  #currentData: Record<PropertyKey, unknown> = {};
+  #startTime: Date;
+
+  constructor(
+    event: string,
+    defaultLogLevel: pino.Level,
+    logger?: pino.Logger,
+  ) {
+    this.#currentLogLevel = defaultLogLevel;
+    this.#startTime = new Date();
+    if (logger) {
+      this.logger = logger;
+    } else {
+      if (!globalLogger) {
+        globalLogger = pino();
+      }
+      this.logger = globalLogger;
+    }
+    this.log({ event });
+  }
+  [Symbol.dispose]() {
+    const endTime = new Date();
+
+    this.log({
+      startTime: this.#startTime,
+      endTime,
+      totalTime: endTime.getDate() - this.#startTime.getDate(),
+    });
+    switch (this.#currentLogLevel) {
+      case 'trace':
+        this.logger.trace(this.#currentData);
+        break;
+      case 'debug':
+        this.logger.debug(this.#currentData);
+        break;
+      case 'info':
+        this.logger.info(this.#currentData);
+        break;
+      case 'warn':
+        this.logger.warn(this.#currentData);
+        break;
+      case 'error':
+        this.logger.error(this.#currentData);
+        break;
+      case 'fatal':
+        this.logger.fatal(this.#currentData);
+        break;
+    }
+  }
+  log(data: Record<string, unknown>): Logger {
+    // If field exists, convert to array
+    const keys = Object.keys(this.#currentData);
+    const dataKeys = Object.keys(data);
+    const arrayKeys = keys.filter((k) => dataKeys.includes(k));
+    const newData = { ...this.#currentData };
+    if (arrayKeys.length > 0) {
+      arrayKeys.forEach((k) => {
+        let field = newData[k];
+        if (Array.isArray(field)) {
+          field = [...field, data[k]];
+        } else {
+          field = [field, data[k]];
+        }
+        newData[k] = field;
+      });
+      this.#currentData = { ...this.#currentData, ...newData };
+      return this;
+    }
+
+    this.#currentData = { ...this.#currentData, ...data };
+    return this;
+  }
+  trace(data: Record<PropertyKey, unknown>): Logger {
+    this.#currentLogLevel = 'trace';
+    return this.log(data);
+  }
+  debug(data: Record<PropertyKey, unknown>): Logger {
+    this.#currentLogLevel = 'debug';
+    return this.log(data);
+  }
+  info(data: Record<PropertyKey, unknown>): Logger {
+    this.#currentLogLevel = 'info';
+    return this.log(data);
+  }
+  warn(data: Record<PropertyKey, unknown>): Logger {
+    this.#currentLogLevel = 'warn';
+    return this.log(data);
+  }
+  error(data: Record<PropertyKey, unknown>): Logger {
+    this.#currentLogLevel = 'error';
+    return this.log(data);
+  }
+  fatal(data: Record<PropertyKey, unknown>): Logger {
+    this.#currentLogLevel = 'fatal';
+    return this.log(data);
+  }
+}
+
+export default Logger;
